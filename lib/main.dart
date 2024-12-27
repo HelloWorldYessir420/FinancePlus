c3c5b85a56b0fc7b7b93282ca81d55ed093d88e1
BlyatMan221 <119160448+HelloWorldYessir420@users.noreply.github.com>
2024-12-27 14:12:14 +0700

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'transaksi/transaksi.dart';
 import 'wallet/wallet.dart';
+import 'analisis/analytics.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,11 +12,63 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Wallet App',
+      title: 'Financial Tracker',
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: WalletPage(), // Panggil halaman wallet
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  // List of pages for Hotbar navigation
+  final List<Widget> _pages = [
+    TransactionPage(),
+    WalletPage(),
+    AnalyticsPage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.receipt),
+            label: 'Transactions',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.wallet),
+            label: 'Wallet',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.pie_chart),
+            label: 'Analytics',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
     );
   }
 }
